2b4a81036504ea3f228a04754abb6561df3aa7cc
Sara Kim <46702424+sarajkim24@users.noreply.github.com>
2020-04-01 22:30:22 -0700

    QuickCommit

--- CodeRage!/2PlayerMenu.gd
+++ CodeRage!/2PlayerMenu.gd
@@ -2,14 +2,13 @@ extends Control
 
 var scene_path_to_load
 
-#func _ready():
-	#$ButtonsContainer.grab_focus()
-	#for button in $MarginContainer3.get_children():
-#	for button in $ButtonsContainer/VboxContainer/MarginContainer3.get_children():
-#		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
-#	pass
+func _ready():
+	var button = $ButtonsContainer/VBoxContainer/MarginContainer3/ChangeSceneButton
+	button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+	pass
 
 func _on_Button_pressed(scene_to_load):
 	scene_path_to_load = scene_to_load
 	get_tree().change_scene(scene_path_to_load)
+	#get_tree().change_scene("res://TitleScene.tscn")
 	pass
